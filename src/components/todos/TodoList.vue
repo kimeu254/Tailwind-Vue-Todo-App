1012cc8521f1d81e6787a568d53f8b0b16ee19f3
kimeu254 <jimkimeu254@gmail.com>
2022-09-09 16:52:24 +0300

    Added active classes to my tabs and a bit of styling

--- src/components/todos/TodoList.vue
+++ src/components/todos/TodoList.vue
@@ -1,59 +1,62 @@
 <template>
-    <div>
-      <div class="container mx-auto">
-        <ul class="flex justify-center space-x-2 text-white ">
-          <li>
-            <button
-              @click="currentTab(1)"
-              class="inline-block px-4 py-2 bg-blue-500 focus:outline-none"
-            >
-              All
-            </button>
-          </li>
-          <li>
-            <button
-              @click="currentTab(2)"
-              class="inline-block px-4 py-2 bg-blue-500 focus:outline-none"
-            >
-              Active
-            </button>
-          </li>
-          <li>
-            <button
-              @click="currentTab(3)"
-              class="inline-block px-4 py-2 bg-blue-500 focus:outline-none"
-            >
-              Complete
-            </button>
-          </li>
-        </ul>
-        <div class="p-3 mt-6 bg-white">
-          <div v-if="tab === 1">
-            <AllTask/>
-          </div>
-          <div v-if="tab === 2">
-            <ActiveTasks/>
-          </div>
-          <div v-if="tab === 3">
-            <Complete/>
+  <div class="flex flex-wrap">
+    <div class="w-full">
+      <ul class="flex mb-0 list-none flex-wrap pt-3 pb-4 flex-row">
+        <li class="-mb-px mr-2 last:mr-0 flex-auto text-center">
+          <a class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal" v-on:click="toggleTabs(1)" v-bind:class="{'text-sky-500 bg-white': openTab !== 1, 'text-white bg-sky-500': openTab === 1}">
+            All
+          </a>
+        </li>
+        <li class="-mb-px mr-2 last:mr-0 flex-auto text-center">
+          <a class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal" v-on:click="toggleTabs(2)" v-bind:class="{'text-sky-500 bg-white': openTab !== 2, 'text-white bg-sky-500': openTab === 2}">
+            Active
+          </a>
+        </li>
+        <li class="-mb-px mr-2 last:mr-0 flex-auto text-center">
+          <a class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal" v-on:click="toggleTabs(3)" v-bind:class="{'text-sky-500 bg-white': openTab !== 3, 'text-white bg-sky-500': openTab === 3}">
+            Complete
+          </a>
+        </li>
+      </ul>
+      <div class="relative flex flex-col min-w-0 break-words w-full mb-6">
+        <div class="px-4 py-5 flex-auto">
+          <div class="tab-content tab-space">
+            <div v-bind:class="{'hidden': openTab !== 1, 'block': openTab === 1}">
+              <AllTask/>
+            </div>
+            <div v-bind:class="{'hidden': openTab !== 2, 'block': openTab === 2}">
+              <ActiveTasks/>
+            </div>
+            <div v-bind:class="{'hidden': openTab !== 3, 'block': openTab === 3}">
+              <Complete/>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </template>
+  </div>
+</template>
   
-  <script>
-import { ref } from 'vue';
+<script>
 import AllTask from './AllTask.vue';
 import ActiveTasks from './ActiveTasks.vue';
 import Complete from './Complete.vue';
 
 export default {
     setup() {
-        const tab = ref(1);
-        const currentTab = (tabNumber) => (tab.value = tabNumber);
-        return { tab, currentTab };
+        
+        return {};
     },
-    components: { AllTask, ActiveTasks, Complete }
+    components: { AllTask, ActiveTasks, Complete },
+    data() {
+      return {
+        openTab: 1
+      }
+    },
+    methods: {
+    toggleTabs: function(tabNumber){
+      this.openTab = tabNumber
+    }
+  }
 };
   </script>
